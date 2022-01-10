24c7e7a47491dba115d2f3df722277cbada79c1d
Sebastian Kazimierski <sebastiankazimierski@o2.pl>
2022-01-10 13:44:27 +0100

    changing the layout

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -44,7 +44,7 @@ class DatabaseSeeder extends Seeder
             'category_id' => $family->id,
             'title' => 'My family post',
             'slug' => 'my-first-post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet</p>',
+            'excerpt' => 'Lorem ipsum dolor sit amet',
             'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
             ]);
 
@@ -55,7 +55,7 @@ class DatabaseSeeder extends Seeder
             'category_id' => $work->id,
             'title' => 'My work post',
             'slug' => 'my-work-post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet</p>',
+            'excerpt' => 'Lorem ipsum dolor sit amet',
             'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
             ]);
 
@@ -65,7 +65,7 @@ class DatabaseSeeder extends Seeder
             'category_id' => $personal->id,
             'title' => 'My personal post',
             'slug' => 'my-personal-post',
-            'excerpt' => '<p>Lorem ipsum dolor sit amet</p>',
+            'excerpt' => 'Lorem ipsum dolor sit amet',
             'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
             ]);
         }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -13,8 +13,8 @@
                 </h1>
 
                 <p>
-                    <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
-                </p>
+                    By <a href="/authors/{{ $post ->author-> id}}">{{$post -> author -> name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
+                  </p>
 
 
 
